b7a5ff40a4ff456d5378c9c15a83b7e9cbd1f32d
draintheshallows <kli@quilhealth.com>
2020-03-18 20:49:44 -0400

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Directory from './components/DirectoryComponent';
 
 class App extends Component {
     render() {
@@ -10,6 +11,7 @@ class App extends Component {
                 <div className="container">
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
+                <Directory />
                 </Navbar>
             </div>
         );
