7f2e4f7f9b0f9c0190d73217fc00f201394a358d
draintheshallows <kli@quilhealth.com>
2020-03-26 19:46:49 -0400

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 import App from './App';
