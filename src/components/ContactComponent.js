9994691e59c20a9e33f65aeb51b7f22411ce0379
draintheshallows <kli@quilhealth.com>
2020-03-26 22:24:26 -0400

    first commit

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 function Contact(props) {
     return (
         <div className="container">
             <div className="row">
                 <div className="col">
+                <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    </Breadcrumb>
+                    <hr />
                     <h2>Contact Us</h2>
                     <hr />
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -38,6 +38,17 @@ class Main extends Component {
             );
         }
 
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === 
+                    +match.params.campsiteId)[0]}
+                    comments= {this.state.comments.filter(comment => comment.campsiteId ===
+                    +match.params.campsiteId)}
+                />
+            );
+        }
+
         return (
             <div>
                 <Header />
@@ -45,6 +56,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} 
                     />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact}/>
                     <Redirect to='/home' />
                 </Switch>
