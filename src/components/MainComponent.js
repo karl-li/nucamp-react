0264198031967ccf934c4d83c58d53cbb122136a
draintheshallows <kli@quilhealth.com>
2020-03-26 20:55:33 -0400

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
+
 import {CAMPSITES} from '../shared/campsites';
+import {COMMENTS} from '../shared/comments';
+import {PARTNERS} from '../shared/partners';
+import {PROMOTIONS} from '../shared/promotions';
+
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent'
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -13,6 +19,9 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -20,7 +29,12 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                
+                />
             );
         }
 
@@ -31,6 +45,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} 
                     />} />
+                    <Route exact path='/contactus' component={Contact}/>
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
