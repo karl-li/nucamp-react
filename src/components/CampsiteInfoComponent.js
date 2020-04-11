961035d87754b42e4da954011a19c254d39efb06
draintheshallows <kli@quilhealth.com>
2020-04-11 08:43:33 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,7 +22,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if(comments) {
         return(
             <div className="col-md-5 m-1">
@@ -36,7 +36,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
             );
         }
@@ -81,7 +81,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
@@ -106,10 +106,10 @@ class CommentForm extends Component {
     // }
 
     handleSubmit = values => {
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.toggleModal();
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
         console.log('Current state is: ' + JSON.stringify(values));
         alert('Current state is: ' + JSON.stringify(values));
-        this.toggleModal();
     }
 
     toggleModal = () => {
